69529082529f00f207e78977cee15488c30f7f85
Michael Yu <ItzWarty@gmail.com>
2014-07-22 22:04:25 -0700

    Added Protobuf and Reflection learning tests.

---
+++ libdipc.Tests/ProtobufLearningTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProtoBuf;
+
+namespace libdipc.Tests
+{
+   [TestClass]
+   public class ProtobufLearningTest
+   {
+      [TestInitialize]
+      public void Setup()
+      {  
+      }
+
+      [TestMethod]
+      public void RunProtobufLearningTestByteArraySerialization()
+      {
+         var ms = new MemoryStream();
+         var data = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+         Serializer.Serialize<byte[]>(ms, data);
+         ms.Position = 0;
+         var nextData = Serializer.Deserialize<byte[]>(ms); // doesn't work with vague object
+         Console.WriteLine(nextData.GetType());
+         Console.WriteLine(nextData);
+      }
+
+      [TestMethod]
+      public void RunProtobufLearningTestGenericsSerialization()
+      {
+         var ms = new MemoryStream();
+         var data = new TestGenericClass<string>();
+         Serializer.Serialize<ITestGenericClass<string>>(ms, data);
+         ms.Position = 0;
+         var nextData = Serializer.Deserialize<ITestGenericClass<string>>(ms); // doesn't work with vague object
+         Console.WriteLine(nextData.GetType());
+         Console.WriteLine(nextData);
+      }
+
+      private interface ITestGenericClass<T> where T : class
+      {
+         [ProtoMember(1)]
+         byte[] Values { get; set; }
+
+         [ProtoMember(2)]
+         T Herp { get; set; }
+      }
+
+      [ProtoContract]
+      private class TestGenericClass<T> : ITestGenericClass<T> where T : class
+      {
+         [ProtoMember(1)]
+         public byte[] Values { get; set; }
+
+         [ProtoMember(2)]
+         public T Herp { get; set; }
+      }
+   }
+}
---
+++ libdipc.Tests/ReflectionLearningTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace libdipc.Tests
+{
+   [TestClass]
+   public class ReflectionLearningTests
+   {
+      [TestInitialize]
+      public void Setup()
+      {
+      }
+
+      [TestMethod]
+      public void GenericTypeDefinitionIsAThing()
+      {
+         var type = typeof(GenericClass<,>);
+         Assert.IsTrue(type.IsGenericType);
+         Assert.IsTrue(type.IsGenericTypeDefinition);
+         Assert.IsFalse(type.IsGenericParameter);
+         //foreach (var argument in type.GetGenericArguments())
+         //   Console.WriteLine("> " + argument);
+      }
+
+      [TestMethod]
+      public void GenericTypeDefinitionIsntAlwaysTrue()
+      {
+         var type = typeof(GenericClass<int, float>);
+         Assert.IsTrue(type.IsGenericType);
+         Assert.IsFalse(type.IsGenericTypeDefinition);
+         Assert.IsFalse(type.IsGenericParameter);
+         //foreach (var argument in type.GetGenericArguments())
+         //   Console.WriteLine("> " + argument);
+      }
+
+      [TestMethod]
+      public void Run()
+      {
+         var a = new GenericClass<List<int>, float>();
+      }
+   }
+
+   public class GenericClass<TParam1, TParam2>
+   {
+      public TParam1 Field1 { get; set; }
+      public TParam2 Field2 { get; set; }
+      public Tuple<TParam1, TParam2> CombinationField1 { get; set; }
+   }
+}
