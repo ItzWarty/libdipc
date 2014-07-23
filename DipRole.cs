27b8ccb3ccd37cd04f830f3a0a242422ef9fc9af
Michael Yu <ItzWarty@gmail.com>
2014-07-22 22:03:50 -0700

    Reallocated DipRole bits, introduced DipRole.Service flag.

--- DipRole.cs
+++ DipRole.cs
@@ -9,14 +9,16 @@ namespace Dargon.Ipc
    [Flags]
    public enum DipRole : uint
    {
-      Local          = 0x00010000U,
-      Remote         = 0x00020000U,
-      Host           = 0x00040000U,
-      Global         = 0x00080000U,
+      Local          = 0x00000001U,
+      Remote         = 0x00000002U,
+      Host           = 0x00000004U,
+      Global         = 0x00000008U,
       
-      Terminal       = 0x00000001U,
-      Router         = 0x00000002U,
-      Network        = 0x00000004U,
+      Terminal       = 0x00000100U,
+      Router         = 0x00000200U,
+      Network        = 0x00000400U,
+
+      Service        = 0x00010000U,
 
       LocalTerminal  = Local | Terminal,
       LocalRouter    = Local | Router,
