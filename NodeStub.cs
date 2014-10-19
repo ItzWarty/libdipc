86d43e6a42ef94d4b8b2b57baacf71541356e3b0
Michael Yu <ItzWarty@gmail.com>
2014-10-19 16:55:10 -0700

    Added NodeStub, which represents a node with a Guid.

---
+++ NodeStub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dargon.Ipc
+{
+   public class NodeStub : INode
+   {
+      private readonly Guid guid;
+
+      public NodeStub(Guid guid) {
+         this.guid = guid;
+      }
+
+      public Guid Guid { get { return guid; } }
+   }
+}
---
+++ libdipc.Tests/NodeStubTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NMockito;
+
+namespace Dargon.Ipc
+{
+   [TestClass]
+   public class NodeStubTests : NMockitoInstance
+   {
+      private NodeStub testObj;
+
+      private readonly Guid guid = Guid.NewGuid();
+
+      [TestInitialize]
+      public void Setup()
+      {
+         testObj = new NodeStub(guid);
+      }
+
+      [TestMethod]
+      public void GuidReflectsConstructorParameterTest()
+      {
+         AssertEquals(guid, testObj.Guid);
+         VerifyNoMoreInteractions();
+      }
+   }
+}
